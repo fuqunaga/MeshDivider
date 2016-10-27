e80a6df708b8a32ae0e3f73d547ed107a47c4f9f
fuqunaga <fukhid@gmail.com>
2016-10-27 18:42:31 +0900

    Create README.md
    add Sample. support normal,uv2
    
    git lfs track * obj
    
    change sample model to small

---
+++ Assets/MeshDivider/Editor/MeshDividerWindow.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MeshDivider
+{
+
+    public class MeshDividerWindow : ScriptableWizard
+    {
+        public string _outputPath = "Assets/MeshDivider";
+        public string _outputName = "MeshDivided";
+        public List<Mesh> _meshs = new List<Mesh>();
+        public Vector3 _gridSize = new Vector3(100f, 100f, 100f);
+
+        [MenuItem("Custom/MeshDivider")]
+        static void Open()
+        {
+            DisplayWizard<MeshDividerWindow>("MeshDivider");
+        }
+
+        protected override bool DrawWizardGUI()
+        {
+            var ret = base.DrawWizardGUI();
+
+            var gridNum = (_meshs != null) ? MeshDividerGrid.CalcGridNum(_meshs, _gridSize) : Vector3.zero;
+
+            var enable = GUI.enabled;
+            GUI.enabled = false;
+            EditorGUILayout.IntField("GridNumTotal", (int)(gridNum.x * gridNum.y * gridNum.z));
+            EditorGUILayout.Vector3Field("GridNum", gridNum);
+            GUI.enabled = enable;
+
+            return ret;
+        }
+
+
+        void OnWizardCreate()
+        {
+            if (_meshs.Any())
+            {
+                const string MESH_FOLDER_NAME = "Mesh";
+
+                var root = new GameObject(_outputName);
+                var standardMat = GetStandardMaterial();
+
+                var meshPath = _outputPath + "/" + MESH_FOLDER_NAME;
+                CreateFolder(meshPath);
+
+                var meshs = MeshDividerGrid.CreateDivideMesh(_meshs, _gridSize);
+                foreach(var mesh in meshs)
+                {
+                    var go = new GameObject("Mesh");
+                    var mf = go.AddComponent<MeshFilter>();
+                    mf.mesh = mesh;
+                    go.AddComponent<MeshRenderer>().sharedMaterial = standardMat;
+                    go.transform.SetParent(root.transform);
+                    
+                    var meshPathUniq = AssetDatabase.GenerateUniqueAssetPath(meshPath + "/ " + _outputName + ".asset");
+                    AssetDatabase.CreateAsset(mesh, meshPathUniq);
+                }
+
+
+                PrefabUtility.SaveAsPrefabAsset(root, _outputPath + "/" + _outputName + ".prefab");
+
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+                EditorUtility.FocusProjectWindow();
+            }
+        }
+
+
+        Material GetStandardMaterial()
+        {
+            var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var ret = go.GetComponent<MeshRenderer>().sharedMaterial;
+            DestroyImmediate(go);
+            return ret;
+        }
+
+        void CreateFolder(string path)
+        {
+            path.Split('/').Aggregate("", (currentPath, folderName) =>
+            {
+                var ret = currentPath + (string.IsNullOrEmpty(currentPath) ? "" : "/") + folderName;
+                if (!AssetDatabase.IsValidFolder(ret))
+                    AssetDatabase.CreateFolder(currentPath, folderName);
+                return ret;
+            });
+        }
+
+#if false
+        void OnFocus()
+        {
+            SceneView.onSceneGUIDelegate -= OnSceneGUI;
+            SceneView.onSceneGUIDelegate += OnSceneGUI;
+        }
+
+        void OnLostFocus()
+        {
+            OnDestroy();
+        }
+
+        void OnDestroy()
+        {
+            SceneView.onSceneGUIDelegate -= OnSceneGUI;
+        }
+
+        void OnSceneGUI(SceneView sceneView)
+        {
+            var bounds = new Bounds(Vector3.zero, Vector3.one * 10f);
+            _meshs.ForEach((mesh) => {
+                if (mesh)
+                {
+                    bounds.Encapsulate(mesh.bounds);
+                    Graphics.DrawMeshNow(mesh, Vector3.zero, Quaternion.identity);
+                }
+            });
+
+            var size = bounds.size;
+            var gridNum = new Vector3(
+                Mathf.Floor(size.x / _gridSize.x) + 1f,
+                Mathf.Floor(size.y / _gridSize.y) + 1f,
+                Mathf.Floor(size.z / _gridSize.z) + 1f
+                );
+
+            var isGirdNumClamped = false;
+            for (var i = 0; i < 3; ++i)
+            {
+                // 多いと重いので適当に少なくする
+                var max = 100f;
+                if (gridNum[i] > max)
+                {
+                    gridNum[i] = Mathf.Clamp(gridNum[i], 0f, max);
+                    isGirdNumClamped = true;
+                }
+            }
+
+			var col = isGirdNumClamped ? Color.red : Color.white;
+
+            for(var i=0; i<3; ++i)
+            {
+                var xRank = i;
+                var yRank = (i + 1) % 3;
+                var zRank = (i + 2) % 3;
+
+                for(var xIdx=0; xIdx<gridNum[xRank]; ++xIdx)
+                {
+                    var x = xIdx * _gridSize[xRank] + bounds.min.x;
+                    for (var yIdy = 0; yIdy < gridNum[yRank]; ++yIdy)
+                    {
+                        var y = yIdy * _gridSize[yRank] + bounds.min.y;
+                        var start = new Vector3();
+                        var end = new Vector3();
+                        start[xRank] = end[xRank] = x;
+                        start[yRank] = end[yRank] = y;
+                        start[zRank] = bounds.min[zRank];
+                        end[zRank]   = bounds.max[zRank];
+                        Debug.DrawLine(start, end, col);
+                    }
+                }
+            }
+        }
+#endif
+    }
+}
---
+++ Assets/MeshDivider/MeshDividerGrid.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace MeshDivider
+{
+
+    public static class MeshDividerGrid
+    {
+        class VtxData
+        {
+            public Mesh mesh;
+            public int idx;
+
+            public Vector3 vtx => meshCache.vertices[idx];
+            public Vector3 normal => meshCache.normals[idx];
+            public Vector2 uv => meshCache.uvs[idx];
+            public Vector2 uv2 => meshCache.uv2s[idx];
+
+            public override int GetHashCode()
+            {
+                return mesh.GetHashCode() ^ idx.GetHashCode();
+            }
+
+            #region MeshCache
+
+            class MeshCache
+            {
+                public Vector3[] vertices;
+                public Vector3[] normals;
+                public Vector2[] uvs;
+                public Vector2[] uv2s;
+            }
+
+            static Dictionary<Mesh, MeshCache> meshToCache = new Dictionary<Mesh, MeshCache>();
+
+            MeshCache meshCache
+            {
+                get
+                {
+                    if (!meshToCache.TryGetValue(mesh, out var cache))
+                    {
+                        meshToCache[mesh] = cache = new MeshCache()
+                        {
+                            vertices = mesh.vertices,
+                            normals = mesh.normals,
+                            uvs = mesh.uv,
+                            uv2s = mesh.uv2
+                        };
+                    }
+                    return cache;
+                }
+            }
+
+            #endregion
+        }
+
+
+        public static Bounds CalcTotalBounds(List<Mesh> _meshs)
+        {
+            return _meshs.Where(mesh => mesh != null).Aggregate(default(Bounds), (b, mesh) => { b.Encapsulate(mesh.bounds); return b; });
+        }
+
+        public static Vector3 CalcGridNum(List<Mesh> _meshs, Vector3 gridSize)
+        {
+            var bounds = CalcTotalBounds(_meshs);
+            var size = bounds.size;
+
+            return new Vector3(
+                Mathf.Floor(size.x / gridSize.x) + 1f,
+                Mathf.Floor(size.y / gridSize.y) + 1f,
+                Mathf.Floor(size.z / gridSize.z) + 1f
+                );
+        }
+
+        public static IEnumerable<Mesh> CreateDivideMesh(List<Mesh> meshs, Vector3 gridSize)
+        {
+            Assert.IsTrue(meshs.SelectMany(mesh => Enumerable.Range(0, mesh.subMeshCount).Select((i) => mesh.GetTopology(i))).All(topology => topology == MeshTopology.Triangles), "MeshDivederGrid allows Only MeshTopology.Triangels.");
+
+            var bounds = CalcTotalBounds(meshs);
+            var gridNum = CalcGridNum(meshs, gridSize);
+
+            Func<Vector3, int> PosToId = (pos_orig) =>
+            {
+                var pos = pos_orig - bounds.min;
+                return (int)(Mathf.Floor(pos.x / gridSize.x) * gridNum.y * gridNum.z
+                + Mathf.Floor(pos.y / gridSize.y) * gridNum.z
+                + Mathf.Floor(pos.z / gridSize.z));
+            };
+
+            var gridTotalNum = (int)(gridNum.x * gridNum.y * gridNum.z);
+
+
+            var gridDatas = new List<VtxData>[gridTotalNum];
+            for (var i = 0; i < gridTotalNum; ++i) gridDatas[i] = new List<VtxData>();
+
+            meshs.ForEach(mesh =>
+            {
+                for (var subMeshIdx = 0; subMeshIdx < mesh.subMeshCount; ++subMeshIdx)
+                {
+                    var vtxs = mesh.vertices;
+                    var orig_idxs = mesh.GetIndices(subMeshIdx);
+                    for (var i = 0; i < orig_idxs.Length; i += 3)
+                    {
+                        var idx = orig_idxs[i];
+                        var vtx0 = vtxs[idx];
+
+                        var id = PosToId(vtx0); // vtx0が属してるgridに三角形を含める
+
+                        var gridData = gridDatas[id];
+
+                        gridData.Add(new VtxData() { mesh = mesh, idx = orig_idxs[i] });
+                        gridData.Add(new VtxData() { mesh = mesh, idx = orig_idxs[i + 1] });
+                        gridData.Add(new VtxData() { mesh = mesh, idx = orig_idxs[i + 2] });
+
+                    }
+                }
+            });
+
+            var hasNormal = meshs.All(m => m.normals.Any());
+            var hasUV = meshs.All(m => m.uv.Any());
+            var hasUV2 = meshs.All(m => m.uv2.Any());
+
+            return gridDatas
+                .Where((gridData) => gridData.Any())
+                .Select(gridData =>
+                {
+                    var vtxs = new List<Vector3>();
+                    var normals = new List<Vector3>();
+                    var uvs = new List<Vector2>();
+                    var uv2s = new List<Vector2>();
+
+
+                    var idxs = new List<int>();
+                    var posToIdx = new Dictionary<VtxData, int>();
+
+                    for (var i = 0; i < gridData.Count; ++i)
+                    {
+                        var vtxData = gridData[i];
+                        int idx = -1;
+                        if (!posToIdx.TryGetValue(vtxData, out idx))
+                        {
+                            vtxs.Add(vtxData.vtx);
+                            if (hasNormal) normals.Add(vtxData.normal);
+                            if (hasUV) uvs.Add(vtxData.uv);
+                            if (hasUV2) uv2s.Add(vtxData.uv2);
+
+                            idx = vtxs.Count - 1;
+
+                            posToIdx[vtxData] = idx;
+                        }
+
+                        idxs.Add(idx);
+                    }
+
+
+                    var mesh = new Mesh();
+                    mesh.vertices = vtxs.ToArray();
+                    if (hasNormal) mesh.normals = normals.ToArray();
+                    if ( hasUV ) mesh.uv = uvs.ToArray();
+                    if (hasUV2) mesh.uv2 = uv2s.ToArray();
+                    mesh.SetIndices(idxs.ToArray(), MeshTopology.Triangles, 0, true);
+
+                    return mesh;
+                });
+        }
+    }
+}
